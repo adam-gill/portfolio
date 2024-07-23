e896d8cf0aad4429527b8faafe17c126accde719
adam-gill <adam.douglas.gill@gmail.com>
2024-07-23 14:56:52 -0400

    add resume view route

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
-import "./App.css"
-import { useEffect } from "react"
-import MouseFollower from "./components/MouseFollower"
-import Header from "./components/Header"
-import About from "./components/About"
-import TechStack from "./components/TechStack"
-import Projects from "./components/Projects"
-import Resume from "./components/Resume"
-import Footer from "./components/Footer"
+import "./App.css";
+import { useEffect } from "react";
+import MouseFollower from "./components/MouseFollower";
+import Header from "./components/Header";
+import About from "./components/About";
+import TechStack from "./components/TechStack";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ResumeView from "./components/ResumeView/ResumeView";
 
 function App() {
   async function initLocomotiveScroll() {
-    const LocomotiveScroll = (await import("locomotive-scroll")).default
-    new LocomotiveScroll()
+    const LocomotiveScroll = (await import("locomotive-scroll")).default;
+    new LocomotiveScroll();
   }
 
   useEffect(() => {
-    initLocomotiveScroll()
-  }, [])
+    initLocomotiveScroll();
+  }, []);
 
   return (
-    <main>
-      <MouseFollower />
-      <Header />
-      <About />
-      <TechStack />
-      <Projects />
-      <Resume />
-      <Footer />
-    </main>
-  )
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <main>
+                <MouseFollower />
+                <Header />
+                <About />
+                <TechStack />
+                <Projects />
+                <Resume />
+                <Footer />
+              </main>
+            </>
+          }
+        />
+        <Route path="/resume" element={<ResumeView />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ResumeView/ResumeView.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./style.css"
+
+const ResumeView = () => {
+    return (
+        <>
+          <h1 className="fcc" >Hey! You found my resume!</h1>  
+          <a href="https://b80ff8907f233f9455352e95a74ac802.r2.cloudflarestorage.com/resumes/resume.pdf">Resume</a>
+         
+
+        </>
+    );
+};
+
+
+export default ResumeView;
